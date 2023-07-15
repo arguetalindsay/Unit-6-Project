e290731b8d4312e4b98edb0c508cf2c0fc5b26dd
aylinargueta <21744484-aylinargueta@users.noreply.replit.com>
2023-07-15 22:53:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Declare your genre arrays here
+
+// Make sure to declare your HTML elements as variables! 
+
+// Submit Button 
+let submitButton = document.querySelector("button");
+submitButton.onclick = function() {
+};
+
